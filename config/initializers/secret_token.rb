abac0a3f8f28022adf349f1ff469b3fd68a4872f
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-06-03 19:26:36 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirbnbTools::Application.config.secret_token = '1a7558118bc991f2ad6ae93fb340a9cad44d9219c8c8d218307be9acdbd3f58a23e24faac78784043c21eb4b825f9a6c8a387864b4ad1f8bd8ba43f55368f269'
