afd0a02111b3780f0473830f61619ffd2bc92583
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-07-24 12:03:25 -0400

    wip

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -9,3 +9,18 @@ describe Airbnb::User do
   its(:first_name) { should == 'Kara' }
   its(:id)         { should == 3241277 }
 end
+
+describe Airbnb::User, '#listings' do
+  subject { Airbnb::User.find(3241277) }
+
+  before do
+    stub_get(:path => '/api/v1/users/3241277', :file => 'user')
+    stub_get(:path => '/api/v1/users/3241277', :file => 'user')
+  end
+
+  it 'returns listings the user owns' do
+    subject.listings[0].name.should == 'My Listing'
+    subject.listings[1].name.should == 'My Other Listing'
+
+  end
+end
