e4cfdfaa797ca5e4ab0e55a711d9b9cbfe6b4bc1
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-10-09 01:17:03 -0400

    wip

--- lib/airbnb/connection.rb
+++ lib/airbnb/connection.rb
@@ -1,11 +1,11 @@
 module Airbnb
   module Connection
-    def self.get(path, params = {})
+    def self.get(path, params = nil)
       uri              = Addressable::URI.new
       uri.host         = 'm.airbnb.com'
       uri.scheme       = 'https'
       uri.path         = "api/v1#{path}"
-      uri.query        = params[:query].try(:to_query)
+      uri.query        = params.try(:to_query)
       response = RestClient.get(uri.to_s).body
       Hashie::Mash.new(JSON.parse(response)) rescue response
     end
