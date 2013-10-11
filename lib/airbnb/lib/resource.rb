ea3fd8457507dfeb56d1802bb537e085e46ab1e2
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-10-11 17:12:28 -0400

    wip

--- lib/airbnb/lib/resource.rb
+++ lib/airbnb/lib/resource.rb
@@ -4,39 +4,39 @@ module Airbnb
       extend ActiveSupport::Concern
 
       included do
-        cattr_accessor :_base_name, :_search_options, :_default_search_options
+        cattr_accessor :base_name, :search_options, :default_search_options
 
-        self._base_name              = self.name.split('::').last.downcase
-        self._search_options         = []
-        self._default_search_options = Hashie::Mash.new
+        self.base_name              = self.name.split('::').last.downcase
+        self.search_options         = []
+        self.default_search_options = Hashie::Mash.new
 
         include Lib::Routes
-        resources _base_name
+        resources base_name
       end
 
       module ClassMethods
 
         def search_option(parameter, options = {})
-          self._search_options << parameter.to_s
-          self._default_search_options[parameter] = options[:default] if options.has_key?(:default)
+          self.search_options << parameter.to_s
+          self.default_search_options[parameter] = options[:default] if options.has_key?(:default)
         end
 
         def find(id)
-          new get(self.class.send(:"#{self._base_name}_path", id))[self._base_name]
+          new get(self.class.send(:"#{self.base_name}_path", id))[self.base_name]
         end
 
         def count(params = {})
-          data(params)["#{_base_name}s_count"]
+          data(params)["#{base_name}s_count"]
         end
 
         def fetch(params = {})
-          data(params)["#{_base_name}s"].map do |record|
-            new(record["#{_base_name}"])
+          data(params)["#{base_name}s"].map do |record|
+            new(record["#{base_name}"])
           end
         end
 
         def data(params = {})
-          options  = self._default_search_options.merge(params)
+          options  = self.default_search_options.merge(params)
 
           per_page = params[:per_page] || 10
           page     = params[:page]     || 1
@@ -45,14 +45,14 @@ module Airbnb
           options[:offset]         = offset
           options[:items_per_page] = per_page
 
-          options = sanatize options, self._search_options
+          options = sanatize options, self.search_options
 
-          self.get(self.send(:"#{self._base_name}s_path"), options)
+          self.get(self.send(:"#{self.base_name}s_path"), options)
         end
       end
 
       def update
-        write_attributes get(self.class.send(:"#{self.class._base_name}_path", self))[self.class._base_name]
+        write_attributes get(self.class.send(:"#{self.class.base_name}_path", self))[self.class.base_name]
       end
 
       def write_attributes(attributes = {})
