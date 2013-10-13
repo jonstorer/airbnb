999b104100f332cee5c4b081cacaf97b1f247483
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-10-13 00:59:01 -0400

    set wait value for api calls

--- lib/airbnb/connection.rb
+++ lib/airbnb/connection.rb
@@ -1,13 +1,28 @@
 module Airbnb
   module Connection
+
     def self.get(path, params = nil)
       uri              = Addressable::URI.new
       uri.host         = 'm.airbnb.com'
       uri.scheme       = 'https'
       uri.path         = "api/-/v1#{path}"
       uri.query        = params.try(:to_query)
+
+      wait
+
       response = RestClient.get(uri.to_s).body
       Hashie::Mash.new(JSON.parse(response)) rescue response
+    end
+
+    private
+
+    def self.wait
+      sleep @wait.to_i
+    end
+
+    def self.wait_for(value=nil)
+      @wait = value if value
+      @wait
     end
   end
 end
