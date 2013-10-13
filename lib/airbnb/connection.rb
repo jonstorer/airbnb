c4fd86d08b07de803bc554d0c850880a921a0cbe
Jonathon Storer <jonathon.scott.storer@gmail.com>
2013-10-13 01:08:20 -0400

    Configuration block for setting the wait time
    closes #12

--- lib/airbnb/connection.rb
+++ lib/airbnb/connection.rb
@@ -1,13 +1,29 @@
 module Airbnb
   module Connection
+
     def self.get(path, params = nil)
       uri              = Addressable::URI.new
       uri.host         = 'm.airbnb.com'
       uri.scheme       = 'https'
       uri.path         = "api/-/v1#{path}"
       uri.query        = params.try(:to_query)
+
+      # TODO: test
+      wait
+
       response = RestClient.get(uri.to_s).body
       Hashie::Mash.new(JSON.parse(response)) rescue response
+    end
+
+    private
+
+    # TODO: test
+    def self.wait
+      sleep @wait.to_i
+    end
+
+    def self.wait_for(value=nil)
+      @wait = value.to_i
     end
   end
 end
